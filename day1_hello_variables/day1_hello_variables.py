c38d8d481f0451649bf4838545c7e2da5dbb5f3f
eraldjustinsanchez-byte <100-days-of-code>
2026-03-04 00:32:20 +0800

    Day 1: Hello_Variables

---
+++ day1_hello_variables/day1_hello_variables.py
@@ -0,0 +1,26 @@
+print("Hello World!")
+print("My name is Erald")
+print("I will be a Full Stack + Data Engineer")
+
+#variables
+name="Erald" #string
+age=25 #int
+height=172.72 #float
+is_learning=True #bool
+
+#fstrings
+print(f"my name is {name}, I'm {age} years old, my height is {height} cm") 
+
+favorite_food="Kebab"
+dream_job="Data Engineering and a Full Stack"
+daily_study_hours=8
+
+print(f"I love eating {favorite_food}, I want to become a {dream_job}, and I study for {daily_study_hours}")
+
+#Inputs
+num1=int(input("Enter the first number: "))
+num2=int(input("Enter the second number: "))
+
+sum_result= num1 + num2
+
+print(f"The sum is {sum_result}")
