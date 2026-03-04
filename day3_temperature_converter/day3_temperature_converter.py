efd9231de97b5e44609673b22eabe0bbcf3138b9
eraldjustinsanchez-byte <100-days-of-code>
2026-03-04 23:09:08 +0800

    day3_temperature_converter

---
+++ day3_temperature_converter/day3_temperature_converter.py
@@ -0,0 +1,24 @@
+while True:
+
+    print("\n=== Temperature Converter ===")
+
+    temperature = float(input("Enter temperature: "))
+    unit = input("Convert from (C/F): ")
+
+    if unit.upper() == "C":
+        result = (temperature * 9/5) + 32
+        print(f"{temperature}°C = {result}°F")
+
+    elif unit.upper() == "F":
+        result = (temperature - 32) * 5/9
+        print(f"{temperature}°F = {result}°C")
+
+    else:
+        print("Invalid unit.")
+
+    again = input("Convert again? (yes/no): ")
+
+    if again.lower() != "yes":
+        break
+
+print("Converter closed.")
