f884f16fd7081c27c1403492d467db116c60be45
eraldjustinsanchez-byte <eraldjustinsanchez@gmail.com>
2026-03-19 23:43:05 +0800

    feat: day 18 unit converter CLI; refactor: day 15 expense tracker CLI

---
+++ original/day18_unit_converter_CLI/constants.py
@@ -0,0 +1,20 @@
+# constants.py
+
+LENGTH_UNITS = {
+    "m": 1,
+    "km": 1000,
+    "cm": 0.01,
+    "mm": 0.001
+}
+
+WEIGHT_UNITS = {
+    "kg": 1,
+    "g": 0.001,
+    "lb": 0.453592
+}
+
+TIME_UNITS = {
+    "s": 1,
+    "min": 60,
+    "hr": 3600
+}
